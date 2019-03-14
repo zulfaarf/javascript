884b359d6f730fa9ae8d8180ee49a4d9abd2d1a5
zulfaarf <zulfaarifah07@gmail.com>
2019-03-14 11:00:21 +0700

    Add funtion

---
+++ function.js
@@ -0,0 +1,16 @@
+function jalansatu() {
+  console.log("Jalan Satu");
+}
+
+function jalandua() {
+  console.log("Jalan Dua");
+}
+
+jalansatu()
+jalandua()
+
+const jalantiga = ()=> {
+  console.log("Jalan Tiga")
+}
+
+jalantiga()
