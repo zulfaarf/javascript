b1cb6dfe775cb38500e941665bcea7468ae2cc79
zulfaarf <zulfaarifah07@gmail.com>
2019-03-15 08:46:40 +0700

    Add if-else

---
+++ if-else.js
@@ -0,0 +1,14 @@
+function jalansatu() {
+  var nilai = 2;
+  // if(nilai === 3) {
+  //   console.log("Benar")
+  // }else if (nilai === 2) {
+  //   console.log("Dua")
+  // }else {
+  //   console.log("Salah")
+  // }
+  if(nilai === 1)console.log("OK")
+  else console.log("Bukan")
+}
+
+jalansatu()
---
+++ switch.js
@@ -0,0 +1,21 @@
+function testingSwitch() {
+  // var nilai = 10;
+  // var nilai = 6;
+  var nilai = "Dilo";
+
+  switch(nilai) {
+    case 10:
+      console.log("Sangat Bagus");
+      break;
+    case 8:
+      console.log("Baik");
+      break;
+    case 6:
+      console.log("Cukup");
+      break;
+    default:
+      console.log("Lainnya")
+    }
+  }
+
+  testingSwitch()
